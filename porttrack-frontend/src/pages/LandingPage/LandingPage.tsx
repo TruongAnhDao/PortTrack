06718a30dd72e50b6f720201bcd6336821532bb1
Dao Truong Anh <truongandao16@gmail.com>
2026-04-30 22:32:07 +0700

    Fix khoảng cáh LandingPage

--- porttrack-frontend/src/pages/LandingPage/LandingPage.tsx
+++ porttrack-frontend/src/pages/LandingPage/LandingPage.tsx
@@ -22,7 +22,7 @@ export const LandingPage: React.FC = () => {
       <Header />
       
       {/* Khối main được flex-grow để đẩy Footer xuống đáy nếu nội dung ngắn */}
-      <main className="flex-grow pt-20 pb-16">
+      <main className="flex-grow pt-10 pb-16">
         
         {/* HERO SECTION */}
         <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 lg:py-24">
